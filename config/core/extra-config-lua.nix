92c48353620073d686a16b62b7e9a427558b7f8f
y3owk1n <wongyeowkin@gmail.com>
2024-01-14 00:37:51 +0800

    chore: standardize error icons

--- config/core/extra-config-lua.nix
+++ config/core/extra-config-lua.nix
@@ -30,5 +30,12 @@
         return orig_util_open_floating_preview(contents, syntax, opts, ...)
     end
 
+    -- Change error sign
+    local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+    for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    end
+
   '';
 }
