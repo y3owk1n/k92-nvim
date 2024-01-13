546ff2e4459519807a0b79009c2594326e524a32
y3owk1n <wongyeowkin@gmail.com>
2024-01-13 08:35:17 +0800

    chore: remove some default provider

--- config/core/globals.nix
+++ config/core/globals.nix
@@ -4,5 +4,8 @@
     maplocalleader = "\\";
     autoformat = true;
     markdown_recommended_style = 0;
+    loaded_ruby_provider = 0;
+    loaded_perl_provider = 0;
+    loaded_python_provider = 0;
   };
 }
